12031213c5b1cf5f039fb12b009369a7f09c512f
Sayyadsulthan <asulthan088@gmail.com>
2024-07-17 14:25:14 +0530

    updated the transactionRoutes

--- routes/api/transactionRoutes.js
+++ routes/api/transactionRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {
     createTransaction,
     getTransactionStatus,
+    processTransaction,
     refundTransaction,
 } from '../../controllers/transactionController';
 
