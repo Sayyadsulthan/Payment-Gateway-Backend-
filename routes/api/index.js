3b2ec036282c91cfd99b18b614dd59c0a3b1a715
Sayyadsulthan <asulthan088@gmail.com>
2024-07-17 14:34:45 +0530

    Added the auth middleware for the authentication of transaction routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import transactionRoutes from './transactionRoutes.js';
+import userRoutes from './userRoutes.js';
 const router = express.Router();
 
 router.use('/auth', userRoutes);
--- routes/api/transactionRoutes.js
+++ routes/api/transactionRoutes.js
@@ -4,13 +4,14 @@ import {
     getTransactionStatus,
     processTransaction,
     refundTransaction,
-} from '../../controllers/transactionController';
+} from '../../controllers/transactionController.js';
+import auth from '../../middlewares/auth.js';
 
 const router = express.Router();
 // TODO Authentication
-router.post('/', /* auth,*/ createTransaction);
-router.post('/:transactionId/process', /* auth,*/ processTransaction);
-router.get('/:transactionId', /* auth,*/ getTransactionStatus);
-router.post('/:transactionId/refund', /* auth,*/ refundTransaction);
+router.post('/', auth, createTransaction);
+router.post('/:transactionId/process', auth, processTransaction);
+router.get('/:transactionId', auth, getTransactionStatus);
+router.post('/:transactionId/refund', auth, refundTransaction);
 
 export default router;
